20de0adb4e1eb0c4ef83f4e5135a95de577417b8
HarshasriM <medaharsha17@gmail.com>
2025-08-10 10:44:09 +0530

    fix cookies in backend

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,7 @@ const User = require('../models/User.js');
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  sameSite: 'none',
   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
 };
 
