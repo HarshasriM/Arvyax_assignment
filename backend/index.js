82339f5979e2107e1fac1750616dc2e35f86e01c
HarshasriM <medaharsha17@gmail.com>
2025-08-10 11:01:02 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: '2mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin:  [process.env.CLIENT_URL, "http://localhost:5173"],
   credentials: true
 }));
 
